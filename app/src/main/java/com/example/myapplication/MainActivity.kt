127a02a6d514aa52e1fc0885b3d931e2f4199857
Souhaieb <Souhaieb@DESKTOP-NS47JT2.local>
2019-12-15 16:59:52 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+// the main class used by the android app
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
